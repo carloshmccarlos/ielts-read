fdbabf6c2ddd03b6b2182360a047a5c091e61a55
carloshmccarlos <carloshmccarlos@gmail.com>
2025-07-15 14:02:02 +0800

    123

--- lib/auth/send-email.ts
+++ lib/auth/send-email.ts
@@ -24,7 +24,7 @@ export async function sendEmail({ to, subject, text }: Props) {
 	// Only send the email if we have a valid API key
 	if (resendAPI) {
 		return await resend.emails.send({
-			from: "i-read-offcial@ielts-read.space",
+			from: "i-read-official@ielts-read.space",
 			to: to,
 			subject: subject,
 			html: text,
